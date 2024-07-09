b50d90bff3dfb7797667e403f516f6a9f787c6af
vishsuvaidyam <vish.suvaidyam@gmail.com>
2024-07-09 12:49:32 +0530

    updatefile

--- appointments_management/appointments_management/doctype/appointment/appointment.js
+++ appointments_management/appointments_management/doctype/appointment/appointment.js
@@ -1,25 +1,16 @@
-// Copyright (c) 2024, vish and contributors
-// For license information, please see license.txt
+// // Copyright (c) 2024, vish and contributors
+// // For license information, please see license.txt
+frappe.ui.form.on('Appointment', {
+   
+    refresh: function(frm) {
+        if (frm.fields_dict.date && frm.fields_dict.date.$input) {
+            frm.fields_dict.date.$input.datepicker({
+                minDate: new Date()  
+            });
+        }
+    }
+    
+});
 
-// frappe.ui.form.on('Appointment', {
-//     after_save: function(frm) {
-//         if (frm.doc.contact_number) {
-//             send_sms(frm.doc.contact_number, `Your appointment is confirmed for ${frm.doc.date}.`);
-//         }
-//     }
-// });
 
-// function send_sms(phone_number, message) {
-//     frappe.call({
-//         method: "appointments_management.appointments_management.send_sms",
-//         args: {
-//             phone_number: phone_number,
-//             message: message
-//         },
-//         callback: function(response) {
-//             if (!response.exc) {
-//                 frappe.msgprint(__('SMS sent successfully'));
-//             }
-//         }
-//     });
-// }
+ 
