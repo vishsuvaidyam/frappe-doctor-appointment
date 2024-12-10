3d363665bd2346166e9961fe455de38c3564a1be
vishsuvaidyam <vish.suvaidyam@gmail.com>
2024-12-11 00:06:24 +0530

    add vue js

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+	<h1 class="text-red-500">Home Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  resources: {
+	ping() {
+	  return {
+		method: "frappe.ping", // Method to call on backend
+		onSuccess(d) {
+		  alert(d);
+		},
+	  };
+	},
+  },
+};
+</script>
