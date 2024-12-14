b0e31900e5eb9d60e115b08768b59d28e9022686
vivek620007 <vivekkumar6200168007@gmail.com>
2024-12-14 16:40:10 +0530

    changes

--- frontend/src/views/All_doctors.vue
+++ frontend/src/views/All_doctors.vue
@@ -1,8 +1,12 @@
 <template>
     <div v-for="(doctor, index) in doctors" :key="index"
         class="bg-white shadow-md rounded-lg border-t-4 border-blue-500 p-6 text-center">
-        <img class="w-24 h-24 mx-auto rounded-full border-2 border-blue-500" :src="doctor.image"
+        <div>
+            <img class="w-full h-54 mx-auto " :src="doctor.image"
             :alt="`Image of ${doctor.name}`">
+
+        </div>
+        
         <div class="mt-4">
             <span class="text-green-500 text-sm font-medium">● Available</span>
             <h3 class="text-lg font-semibold text-gray-800 mt-2">{{ doctor.name }}</h3>
