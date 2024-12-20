b4c60ec1ebd4b39f5ff0f08e09317fd61d9b3be3
vishsuvaidyam <vish.suvaidyam@gmail.com>
2024-12-20 16:52:02 +0530

    conflict

--- frontend/src/views/All_doctors.vue
+++ frontend/src/views/All_doctors.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
     <div
         v-for="(doctor, index) in doctors"
         :key="index"
@@ -8,13 +7,6 @@
     >
         <div>
             <img class="w-full h-54 mx-auto rounded" :src="doctor.doctor_image" :alt="`${doctor.full_name[0]}`" />
-=======
-    <div v-for="(doctor, index) in doctors" :key="index"
-        class="bg-white shadow-md rounded-lg border-t-4 border-blue-500 p-6 text-start cursor-pointer"
-        @click="goToDoctorDetails(doctor.full_name)">
-        <div class="">
-            <img class="w-full h-54 mx-auto " :src="doctor.doctor_image" :alt="`${doctor.full_name[0]}`">
->>>>>>> dev_vish
         </div>
         <div class="mt-4">
             <span
