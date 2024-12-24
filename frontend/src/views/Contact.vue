4a2f39cdf27f2c3df3c11ccfa480fd735887808f
vivek620007 <vivekkumar6200168007@gmail.com>
2024-12-24 16:25:42 +0530

    Change the design home

--- frontend/src/views/Contact.vue
+++ frontend/src/views/Contact.vue
@@ -1,37 +1,42 @@
 <template>
-    <div class="w-full pt-10">
-        <h1 class="text-2xl font-normal text-center text-gray-400">CONTACT <span
-                class="text-md text-gray-700 font-extrabold">US</span>
+    <div class="w-full px-5 sm:px-10 lg:px-20 pt-10">
+        <h1 class="text-2xl font-normal text-center text-gray-400">
+            CONTACT <span class="text-md text-gray-700 font-extrabold">US</span>
         </h1>
-        <div class="flex justify-center w-full pt-10 gap-10 ">
-            <div class="w-1/2 flex justify-end">
-                <div>
-                    <img class="w-96 max-h-auto" src="../assets/contact_image-IJu_19v_.png" />
-                </div>
 
+        <div class="flex flex-col sm:flex-row justify-center items-center w-full pt-10 gap-10">
+            <div class="w-full sm:w-1/2 flex justify-center sm:justify-end">
+                <img class="w-full sm:w-96 max-h-auto" src="../assets/contact_image-IJu_19v_.png" />
             </div>
-            <div class="w-1/2 flex justify-start items-center">
-                <div class="w-1/4">
-                    <h1 class="text-md text-gray-600 font-semibold pb-6">OUR OFFICE</h1>
-                    <p class="text-sm font-normal text-gray-500 pb-6">00000 Willms Station <br>
-                        Suite 000, Washington, USA
 
+            <div class="w-full sm:w-1/2 flex justify-center sm:justify-start items-center">
+                <div class="w-full sm:w-3/4">
+                    <h1 class="text-md text-gray-600 font-semibold pb-6">OUR OFFICE</h1>
+                    <p class="text-sm font-normal text-gray-500 pb-6">
+                        00000 Willms Station <br />
+                        Suite 000, Washington, USA
                     </p>
                     <p class="text-sm font-normal text-gray-500 pb-6">Tel: (000) 000-0000 </p>
                     <p class="text-sm font-normal text-gray-500 pb-6">Email: greatstackdev@gmail.com</p>
                     <h1 class="text-md text-gray-600 font-semibold pb-6">CAREERS AT PRESCRIPTO</h1>
-                    <p class="text-sm font-normal text-gray-500 pb-6">Learn more about our teams and job openings.</p>
+                    <p class="text-sm font-normal text-gray-500 pb-6">
+                        Learn more about our teams and job openings.
+                    </p>
                     <button
-                        class="py-4 px-8 text-sm transition-all border-black border hover:bg-black hover:text-white">Explore
-                        Jobs</button>
+                        class="py-2 px-6 text-sm transition-all border-black border hover:bg-black hover:text-white">
+                        Explore Jobs
+                    </button>
                 </div>
             </div>
         </div>
+
+        <!-- Footer Section -->
         <div class="mt-20">
             <Footer />
         </div>
     </div>
 </template>
+
 <script setup>
 import Footer from '../components/Footer.vue';
 </script>
