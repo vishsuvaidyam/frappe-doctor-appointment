b768b2f9beff1bb3323d95e0e54f20b127a840ab
vivek620007 <vivekkumar6200168007@gmail.com>
2024-12-30 16:31:31 +0530

    change the profile

---
+++ frontend/src/views/Realated_doctors.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <!-- Check if there are doctors -->
+        <div v-if="doctors && doctors.length > 0" 
+            class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6 w-full">
+            <div v-for="doctor in doctors" :key="doctor.full_name" 
+                @click="$emit('doctorClicked', doctor)" 
+                class="bg-white shadow-md rounded-lg border-t-4 border-blue-500 p-6 text-start cursor-pointer">
+                <div>
+                    <img class="w-full h-54 mx-auto" :src="doctor.doctor_image" :alt="doctor.full_name" />
+                </div>
+                <div class="mt-4">
+                    <span :class="doctor.status === 'Available' ? 'text-green-500' : 'text-red-500'"
+                        class="text-sm font-medium">
+                        ● {{ doctor.status }}
+                    </span>
+                    <h3 class="text-lg font-semibold text-gray-800 mt-2">
+                        {{ doctor.full_name }}
+                    </h3>
+                    <p class="text-sm text-gray-600">{{ doctor.specialist }}</p>
+                </div>
+            </div>
+        </div>
+        <!-- Fallback message if no doctors are available -->
+        <div v-else class="text-gray-500 text-center">
+            No related doctors found.
+        </div>
+    </div>
+</template>
+
+<script setup>
+ 
+</script>
